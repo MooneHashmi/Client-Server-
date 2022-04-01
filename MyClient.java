13e8655ac3b1ab37e4cffd745a04041ed61e1c13
MooneHashmi <muneebhashmi2015@gmail.com>
2022-04-02 10:17:39 +1100

    Implementation of ATL (refactoring)

--- MyClient.java
+++ MyClient.java
@@ -7,29 +7,80 @@ public class MyClient {
             BufferedReader dis = new BufferedReader(new InputStreamReader(s.getInputStream()));
             //DataInputStream dis=new DataInputStream(s.getInputStream());
             DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-            
+
+
+            // Sending HELO
             dout.write(("HELO\n").getBytes());  
             dout.flush(); 
-            
+
+            // Receiving OK
             String str=(String)dis.readLine();  
-            System.out.println("Server message= "+str);
+            System.out.println("Server message = '" + str + "'");
             dout.flush();
             
-            dout.write(("AUTH user\n").getBytes());  
+            // Sending AUTH
+            dout.write(("AUTH Muneeb\n").getBytes());  
             dout.flush(); 
             
+            // Receiving OK
             str=(String)dis.readLine();  
-            System.out.println("Server message= "+str);
+            System.out.println("Server message = '" + str + "'");
             dout.flush();
             
+            // Sending REDY
             dout.write(("REDY\n").getBytes());  
             dout.flush(); 
+
             
+            // JOBN 37 0 1135 5 1200 6400
+            // GETS Capable 5 1200 6400
+
+
+            str=(String)dis.readLine(); 
+            String[] noneDetector = str.split(" ");
+            if (noneDetector [0] != "NONE" ){
+                String core = noneDetector[-3];
+                String ram = noneDetector[-2];
+                String hardDisk = noneDetector[-1];
+
+                dout.write(("GETS Capable " + core + " " + ram + " " + hardDisk + "\n").getBytes());
+                dout.flush();
+
+            str=(String)dis.readLine(); 
+            String[] data = str.split(" "); 
+
+            dout.write(("OK\n").getBytes());
+            dout.flush();
+
+            str=(String)dis.readLine(); 
+            String[] servers = str.split("\n");
+
+            
+            int maxCore = 0;
+            int serverIDcount = 0;
+            for (int i = 1; i < Integer.parseInt(data[1]); i++){
+
+            String [] maxJob = servers[maxCore].split(" ");
+            String [] currentJob = servers[i].split(" ");
+            
+            if (Integer.parseInt(maxJob[4]) < Integer.parseInt(currentJob[4])){
+                maxCore = i;
+            }
+            if(Integer.parseInt(maxJob[4]) == Integer.parseInt(currentJob[4])){
+                
+            }
+            }
+            dout.write(("OK\n").getBytes());
+            dout.flush();
+            }
+            
+
+
             dout.write(("SCHD 0 super-silk 0\n").getBytes());  
             dout.flush();
             
             str=(String)dis.readLine();  
-            System.out.println("Server message= "+str);
+            System.out.println("Server message = '" + str + "'");
             dout.flush();
             
             
@@ -38,7 +89,7 @@ public class MyClient {
             dout.flush();
 
             str = dis.readLine();  
-            System.out.println("Server message= "+str);
+            System.out.println("Server message = '" + str + "'");
             dout.flush();
 
             dout.close();  
@@ -47,3 +98,5 @@ public class MyClient {
     
     }  
 }  
+
+
