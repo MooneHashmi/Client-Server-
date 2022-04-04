294029aa52eefb9110f0f4c39c22d5d1db71a2f7
MooneHashmi <muneebhashmi2015@gmail.com>
2022-04-05 00:52:09 +1000

    Job Scheduling done

--- MyClient.java
+++ MyClient.java
@@ -1,102 +1,138 @@
-import java.io.*;  
-import java.net.*;  
-public class MyClient {  
-    public static void main(String[] args) {  
-        try{      
-            Socket s=new Socket("localhost",50000);  
-            BufferedReader dis = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            //DataInputStream dis=new DataInputStream(s.getInputStream());
-            DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+import java.io.*;
+import java.net.*;
+
+public class MyClient {
+
+  public static void main(String[] args) {
+    try {
+      Socket s = new Socket("localhost", 50000);
+      BufferedReader dis = new BufferedReader(
+          new InputStreamReader(s.getInputStream()));
+      // DataInputStream dis=new DataInputStream(s.getInputStream());
+      DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+      // Sending HELO
+      dout.write(("HELO\n").getBytes());
+      dout.flush();
+
+      // Receiving OK
+      String str = (String) dis.readLine();
+      System.out.println("Server message = '" + str + "'");
+      dout.flush();
+
+      // Sending AUTH
+      dout.write(("AUTH Muneeb\n").getBytes());
+      dout.flush();
+
+      // Receiving OK
+      str = (String) dis.readLine();
+      System.out.println("Server message = '" + str + "'");
+      dout.flush();
+
+      // Sending REDY
+      dout.write(("REDY\n").getBytes());
+      dout.flush();
+
+      // GETS Capable All
+
+      str = (String) dis.readLine();
+      String[] noneDetector = str.split(" ");
+      System.out.println(noneDetector[0]);
+      System.out.println(str);
+      if (noneDetector[0] != "NONE")
+        dout.write(("GETS All\n").getBytes());
+      dout.flush();
 
 
-            // Sending HELO
-            dout.write(("HELO\n").getBytes());  
-            dout.flush(); 
+      // Receiving Data x x
+      str = (String) dis.readLine();
+      String[] data = str.split(" ");
 
-            // Receiving OK
-            String str=(String)dis.readLine();  
-            System.out.println("Server message = '" + str + "'");
+      // Sending OK
+      dout.write(("OK\n").getBytes());
+      dout.flush();
+
+      String[] servers = new String[Integer.parseInt(data[1])];
+      for (int i = 0; i < Integer.parseInt(data[1]); i++) {
+        str = (String) dis.readLine();
+        servers[i] = str;
+      }
+      // Sending OK
+      dout.write(("OK\n").getBytes());
+      dout.flush();
+
+      int maxServerIndex = 0;
+      for (int i = 1; i < servers.length; i++) {
+        String[] maxServer = servers[maxServerIndex].split(" ");
+        String[] currentServer = servers[i].split(" ");
+        if (Integer.parseInt(maxServer[4]) < Integer.parseInt(currentServer[4])) {
+          maxServerIndex = i;
+        }
+      }
+      int maxServerIDcount = 0;
+      for (int i = 0; i < servers.length; i++) {
+        if (servers[maxServerIndex].split(" ")[0].equals(servers[i].split(" ")[0])) {
+          System.out.println(
+              "Max Server Count: '" + maxServerIDcount + "'");
+          // System.out.println(""Server " + maxServerIDcount + " " + servers[i].split("
+          // ")+);
+        //  maxServerIDcount = maxServerIDcount + 1;
+        }
+      }
+      System.out.println(
+          "Max Server: '" +
+              servers[maxServerIndex] +
+              "Count: " +
+              maxServerIDcount +
+              "'");
+
+      int jobID = 0;
+      int serverID = 0;
+      System.out.println(maxServerIDcount);
+      while (!(noneDetector[0].equals("NONE"))) {
+        //if (jobID != (maxServerIDcount - 1)) {
+          //for (int i = 0; i < maxServerIDcount; i++) {
+            if (noneDetector[0] .equals("JOBN")){
+            dout.write(
+                ("SCHD " + jobID +" " + servers[maxServerIndex].split(" ")[0] +" " + serverID +"\n").getBytes());
             dout.flush();
-            
-            // Sending AUTH
-            dout.write(("AUTH Muneeb\n").getBytes());  
-            dout.flush(); 
-            
-            // Receiving OK
-            str=(String)dis.readLine();  
-            System.out.println("Server message = '" + str + "'");
+            //jobID++;
+            //serverID++;
+            if (maxServerIDcount >= serverID +1){
+              serverID++;
+            } else {
+              serverID = 0;
+            } 
+            jobID++;
+            //dout.write(("REDY\n").getBytes());
             dout.flush();
-            
-            // Sending REDY
-            dout.write(("REDY\n").getBytes());  
-            dout.flush(); 
-
-            
-            // JOBN 37 0 1135 5 1200 6400
-            // GETS Capable 5 1200 6400
-
-
-            str=(String)dis.readLine(); 
-            String[] noneDetector = str.split(" ");
-            if (noneDetector [0] != "NONE" ){
-                String core = noneDetector[-3];
-                String ram = noneDetector[-2];
-                String hardDisk = noneDetector[-1];
-
-                dout.write(("GETS Capable " + core + " " + ram + " " + hardDisk + "\n").getBytes());
-                dout.flush();
-
-            str=(String)dis.readLine(); 
-            String[] data = str.split(" "); 
-
-            dout.write(("OK\n").getBytes());
-            dout.flush();
-
-            str=(String)dis.readLine(); 
-            String[] servers = str.split("\n");
-
-            
-            int maxCore = 0;
-            int serverIDcount = 0;
-            for (int i = 1; i < Integer.parseInt(data[1]); i++){
-
-            String [] maxJob = servers[maxCore].split(" ");
-            String [] currentJob = servers[i].split(" ");
-            
-            if (Integer.parseInt(maxJob[4]) < Integer.parseInt(currentJob[4])){
-                maxCore = i;
-            }
-            if(Integer.parseInt(maxJob[4]) == Integer.parseInt(currentJob[4])){
-                
-            }
-            }
-            dout.write(("OK\n").getBytes());
-            dout.flush();
-            }
-            
+            str = (String) dis.readLine();
+            noneDetector = str.split(" ");
+          }
+          dout.write(("REDY\n").getBytes());
+          dout.flush();
+          str = (String) dis.readLine();
+          noneDetector = str.split(" ");
+          System.out.println(noneDetector[0]);
 
 
-            dout.write(("SCHD 0 super-silk 0\n").getBytes());  
-            dout.flush();
-            
-            str=(String)dis.readLine();  
-            System.out.println("Server message = '" + str + "'");
-            dout.flush();
-            
-            
+        } //else {
+          //jobID = 0;
+       // }
+     // }
 
-            dout.write(("QUIT\n").getBytes());
-            dout.flush();
+      dout.write(("QUIT\n").getBytes());
+      dout.flush();
+      System.out.println("SENT: 'QUIT'");
 
-            str = dis.readLine();  
-            System.out.println("Server message = '" + str + "'");
-            dout.flush();
+      str = (String) dis.readLine();
 
-            dout.close();  
-            s.close();  
-        }catch(Exception e){System.out.println(e);}  
-    
-    }  
-}  
+      dout.close();
+      s.close();
 
-
+      System.exit(0);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+}
